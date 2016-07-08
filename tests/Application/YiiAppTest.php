8fa43f7147825e20fb617b46facb4a7b9fecf7f6
snowz <marcel.patzwahl@gmail.com>
2016-07-08 10:30:48 +0200

    Additional YiiApp tests

--- tests/Application/YiiAppTest.php
+++ tests/Application/YiiAppTest.php
@@ -59,4 +59,51 @@ class YiiAppTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $result);
     }
+
+    public function testCmdAndNonDoubleDashArgs()
+    {
+        $cmd = 'controller';
+        $arg1 = 'param1';
+        $arg2 = 'param2';
+        $expected = sprintf(
+            'php console.php %s %s=something %s=123',
+            $cmd,
+            $arg1,
+            $arg2
+        );
+
+        $result = Command::create()
+            ->yiiApp()
+            ->cmd($cmd)
+            ->arg($arg1, 'something', '')
+            ->arg($arg2, '123', '')
+            ->getRaw();
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testCmdActionAndArgs()
+    {
+        $cmd = 'controller';
+        $action = 'action';
+        $arg1 = 'begin';
+        $arg2 = 'end';
+        $expected = sprintf(
+            'php console.php %s %s --%s=1 --%s=11000000',
+            $cmd,
+            $action,
+            $arg1,
+            $arg2
+        );
+
+        $result = Command::create()
+            ->yiiApp()
+            ->cmd($cmd)
+            ->action($action)
+            ->arg($arg1, '1', '--')
+            ->arg($arg2, '11000000', '--')
+            ->getRaw();
+
+        $this->assertEquals($expected, $result);
+    }
 }
